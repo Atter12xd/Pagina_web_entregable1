e3ec6d5631b6b166d2de0e79286e499e921a5068
Atter12xd <1485275@senati.pe>
2024-09-08 04:22:46 -0500

    importar librerias necesarias

--- python_2.py
+++ python_2.py
@@ -10,3 +10,20 @@ import sys
 if sys.platform == "win32":
     os.system('chcp 65001')  # Cambia la codificación a UTF-8
 
+# Paso 2: Importar Librerías Necesarias
+# import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
