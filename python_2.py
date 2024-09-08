4d38939462424075fe2ead5b6b2776c0babaac5e
Atter12xd <1485275@senati.pe>
2024-09-08 04:26:16 -0500

    Clasificacion con K-Nearest neighbors

--- python_2.py
+++ python_2.py
@@ -62,3 +62,10 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+# Paso 6: Clasificación con K-Nearest Neighbors (KNN)
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+y_pred_knn = knn.predict(X_test)
+accuracy_knn = accuracy_score(y_test, y_pred_knn)
+print(f"Exactitud del modelo KNN: {accuracy_knn:.2f}")
