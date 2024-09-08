c9eb319b2bdb22995d1d7c152bc6203f227a8ee2
Atter12xd <1485275@senati.pe>
2024-09-08 02:06:14 -0500

    Empezando nuevo proyecto para el sistema de recomendacion de productos

--- python_1.py
+++ python_1.py
@@ -1,1 +1,9 @@
-#empezando nuevo proyecto para la pagina web
+# Empezando nuevo proyecto para el sistema de recomendación de productos
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier  # Ejemplo de modelo para recomendaciones
+import matplotlib.pyplot as plt
+import seaborn as sns
+
